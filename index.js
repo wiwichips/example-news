a06398728152c8d84635a154a31471d1e686664f
wiwichips <wpringle@uoguelph.ca>
2020-05-24 10:55:07 -0400

    created a function getCovidFreeHeadlines that returns a list of covid free headlines

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ require('dotenv').config();
 // require the google news api
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI(process.env.API_KEY); // api key fom .env
+
+function containsCovid(article) {
+  return true;
+}
+
+async function getCovidFreeHeadlines(language, country) {
+  // get the headlines
+  const articles = (await newsapi.v2.topHeadlines({ language, country })).articles;
+
+  // reduce to a set of articles to a subset without covid
+  const covidFree = articles.reduce((safe, article) => {
+    // if the article is covid free, add it to the 'safe' arary
+    if (containsCovid(article)) {
+      safe.push(article);
+    }
+    return safe;
+  }, []);
+
+  // return covidFree articles
+  return covidFree;
+}
+
+// get the articls and print them
+getHeadlines('en', 'us')
+  .then((articles) => console.log(articles))
+  .catch((rej) => console.log(rej));
